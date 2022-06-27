4df32477b4c6c944560cc5f09be5e3914d24af24
maiload <101233929+maiload@users.noreply.github.com>
2022-06-28 00:17:07 +0900

    커밋 6/28

--- javatalk/src/main/java/com/java/talk/controller/TalkController.java
+++ javatalk/src/main/java/com/java/talk/controller/TalkController.java
@@ -3,7 +3,9 @@ package com.java.talk.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TalkController {
@@ -36,6 +38,30 @@ public class TalkController {
 		logger.info("채팅 화면");
 		
 		return "thymeleaf/chat";
+	}
+	
+	@GetMapping("/more")
+	public String more() {
+		logger.info("더보기 화면");
+		
+		return "thymeleaf/more";
+	}
+	
+	@GetMapping("/profile")
+	public String profile(@RequestParam String userId
+			, Model model) {
+		logger.info("프로필 화면, userId={}", userId);
+		
+		model.addAttribute("userId", userId);
+		
+		return "thymeleaf/profile";
+	}
+	
+	@GetMapping("/picture")
+	public String picture() {
+		logger.info("프로필 사진 보기");
+		
+		return "thymeleaf/picture";
 	}
 	
 }
